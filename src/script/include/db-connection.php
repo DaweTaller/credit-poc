9d8c43d7ac462f135f48b3a6099565f327ba0584
davidtaller <david.taller@skrz.cz>
2024-02-10 17:24:04 +0100

    Set timezone

--- src/script/include/db-connection.php
+++ src/script/include/db-connection.php
@@ -2,6 +2,8 @@
 
 declare(strict_types = 1);
 
+date_default_timezone_set('Europe/Prague');
+
 $dsnString = sprintf('mysql:host=db;dbname=%s', getenv('DATABASE_NAME'));
 $pdo = new PDO(
     $dsnString,
